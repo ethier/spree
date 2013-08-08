4a07cbb7f8a4f3a2519e862c8d61213c0c26a40e
Derek Ethier <derek.ethier@gmail.com>
2013-08-08 13:24:29 -0400

    Remove stateful dependency

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -18,7 +18,7 @@ module Spree
 
     belongs_to :shipping_method, :class_name => "Spree::ShippingMethod"
 
-    has_many :state_changes, :as => :stateful, :dependent => :destroy
+    has_many :state_changes, :as => :stateful
     has_many :line_items, :dependent => :destroy, :order => "created_at ASC"
     has_many :inventory_units
     has_many :payments, :dependent => :destroy
