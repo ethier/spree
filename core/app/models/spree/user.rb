69f7fccef3fe6e1a5e7d3a33589749640fa176c5
Miller Peterson <miller@gelaskins.com>
2014-01-24 17:20:20 -0500

    Remove auth user model, wipe out contents of core user model
    Consolidating into spree_gelaskins repo

--- core/app/models/spree/user.rb
+++ core/app/models/spree/user.rb
@@ -1,27 +1,4 @@
-# Default implementation of User.  This class is intended to be modified by extensions (ex. spree_auth)
 module Spree
   class User < ActiveRecord::Base
-    include Core::UserBanners
-
-    attr_accessible :email, :password, :password_confirmation
-
-    has_many :orders
-
-    belongs_to :ship_address, :class_name => 'Spree::Address'
-    belongs_to :bill_address, :class_name => 'Spree::Address'
-
-    scope :registered
-
-    def anonymous?
-      false
-    end
-
-    # Creates an anonymous user
-    def self.anonymous!
-      create
-    end
-
-    attr_accessor :password
-    attr_accessor :password_confirmation
   end
 end
